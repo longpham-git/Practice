5cc51d586b8b4daf95aada08f5a483c98ae59e8c
Long Pham <phamcong.hoanlong@gmail.com>
2022-05-29 00:01:19 +0700

    Change increase counter location
    Increase counter location moved to
    right after field = 7
    Also reset field count right away

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -10,19 +10,20 @@ with open('data.txt', 'r') as data:
 valid_cnt = 0
 field_cnt = 0
 min_eyr = 9999
-mandatory_field = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+mandatory_field = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']
 for line in lines:
     if line == '\n':
-        if field_cnt == 7:
-            valid_cnt += 1
         field_cnt = 0
     else:
         line = line.strip()
         for field in mandatory_field:
             if field in line:
                 field_cnt += 1
-                if field == 'eyr':
+                if field == 'eyr:':
                     expire = int(line.split('eyr:')[1].split()[0])
                     min_eyr = expire if expire < min_eyr else min_eyr
+        if field_cnt == 7:
+            valid_cnt += 1
+            field_cnt = 0
 
 print(f'If this year is {min_eyr}, # of valid passport is: {valid_cnt}')
