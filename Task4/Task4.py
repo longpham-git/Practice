a4400fd97de39b9b9ad92d305f5fbe96bf76e48e
Long Pham <phamcong.hoanlong@gmail.com>
2022-05-28 22:52:33 +0700

    Initial commit: A simple solution

---
+++ Task4/Task4.py
@@ -0,0 +1,28 @@
+"""
+ step 1: every new line is a new passport
+ step 2: valid is 8 (7 + optional cid)
+ step 3: If valid '#' increase counter
+ optional: smallest eyr is taken into account as "this" year
+"""
+
+with open('data.txt', 'r') as data:
+    lines = data.readlines()
+valid_cnt = 0
+field_cnt = 0
+min_eyr = 9999
+mandatory_field = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+for line in lines:
+    if line == '\n':
+        if field_cnt == 7:
+            valid_cnt += 1
+        field_cnt = 0
+    else:
+        line = line.strip()
+        for field in mandatory_field:
+            if field in line:
+                field_cnt += 1
+                if field == 'eyr':
+                    expire = int(line.split('eyr:')[1].split()[0])
+                    min_eyr = expire if expire < min_eyr else min_eyr
+
+print(f'If this year is {min_eyr}, # of valid passport is: {valid_cnt}')
