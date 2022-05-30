e4ffc0eb15b01bfe166c09158f561f79f6b733b6
Long Pham <phamcong.hoanlong@gmail.com>
2022-05-30 21:32:55 +0700

    Update algorithm for when down > 1

--- Task3/Task3_part2.py
+++ Task3/Task3_part2.py
@@ -8,7 +8,7 @@ with open('data.txt', 'r') as data:
     lines = data.readlines()
 product = 1
 slopes = ['1-1', '3-1', '5-1', '7-1', '1-2']
-#slopes = ['3-1']
+#slopes = ['1-2']
 del lines[0]
 iter_lines = iter(lines)
 for slope in slopes:
@@ -16,24 +16,23 @@ for slope in slopes:
     right, down = int(slope.split('-')[0]), int(slope.split('-')[1])
     down_slope = down
     tree_counter, next_location = 0, 0
-    iter_lines = iter(lines)
-    for line in iter_lines:
-        while down_slope > 1:
+    for line in lines:
+        if down_slope == 1:
+            line = line.strip()
+            next_location += right
+            extension_factor = int(next_location / len(line))+1
+            if len(line) <= next_location:
+                line = line * extension_factor
+                # print(line)
+                # print(f'location {next_location} is {line[next_location]}')
+            if line[next_location] == '#':
+                # print("hit a tree")
+                tree_counter += 1
+            down_slope = down
+        else:
             down_slope -= 1
-            next(iter_lines)
-        down_slope = down
-        line = line.strip()
-        next_location += right
-        extension_factor = int(next_location / len(line))+1
-        if len(line) <= next_location:
-            line = line * extension_factor
-            print(line)
-            print(f'location {next_location} is {line[next_location]}')
-        if line[next_location] == '#':
-            # print("hit a tree")
-            tree_counter += 1
     print(tree_counter)
     product = product * tree_counter
 
-print(f'Total tree encounter: {product}')
+print(f'Product tree encounter: {product}')
 
